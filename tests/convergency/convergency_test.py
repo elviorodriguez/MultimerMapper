f84d98fe67455c9eb601d164a8792242443463e9
elviorodriguez <elvio.rodriguez4@gmail.com>
2025-09-25 09:25:55 -0300

    Added modules to test the convergence predictor

---
+++ tests/convergency/convergency_test.py
@@ -0,0 +1,168 @@
+
+import os
+
+from utils.logger_setup import configure_logger
+from src.input_check import seq_input_from_fasta, extract_seqs_from_AF2_PDBs, merge_fasta_with_PDB_data
+from src.metrics_extractor import extract_AF2_metrics_from_JSON, generate_pairwise_2mers_df, generate_pairwise_Nmers_df
+from src.ppi_detector import filter_non_int_2mers_df, filter_non_int_Nmers_df
+from src.stoich.stoich_space_exploration import generate_stoichiometric_space_graph
+
+######################################################################
+########################### Configurations ###########################
+######################################################################
+
+Nmers_contacts_cutoff_convergency = 5
+use_dynamic_conv_soft_func = False
+log_level = "info"
+save_pairwise_data = False
+use_names = True
+overwrite = True
+
+# (FPR, PAE_cutoff, N_models_cutoff)
+cutoffs_list = [
+    # FPR = 0.01
+    (0.01,  1.86, 1),
+    (0.01,  1.89, 2),
+    (0.01,  2.70, 3),
+    (0.01,  4.21, 4),
+    (0.01, 10.50, 5),
+    # FPR = 0.05
+    (0.05,  3.09, 1),
+    (0.05,  4.42, 2),
+    (0.05,  7.18, 3),
+    (0.05, 10.44, 4),
+    (0.05, 12.98, 5)
+]
+
+######################################################################
+########################## Input processing ##########################
+######################################################################
+
+def preprocess_data(fasta_file, AF2_2mers, AF2_Nmers, out_path,
+                    use_names = use_names,
+                    overwrite = overwrite,
+                    save_pairwise_data = save_pairwise_data,
+                    log_level = log_level):
+
+    logger = configure_logger(out_path, log_level = log_level)(__name__)
+
+    # FASTA file processing
+    prot_IDs, prot_names, prot_seqs, prot_lens, prot_N = seq_input_from_fasta(
+        fasta_file, use_names, logger = logger)
+
+    # PDB files processing
+    all_pdb_data = extract_seqs_from_AF2_PDBs(AF2_2mers, AF2_Nmers, logger = logger)
+
+    # Combine data
+    merge_fasta_with_PDB_data(all_pdb_data = all_pdb_data,
+                              prot_IDs = prot_IDs,
+                              prot_names = prot_names, 
+                              prot_seqs = prot_seqs,
+                              prot_lens = prot_lens,
+                              prot_N = prot_N,
+                              use_names = use_names,
+                              logger = logger)
+    
+    # Extract AF2 metrics
+    sliced_PAE_and_pLDDTs = extract_AF2_metrics_from_JSON(all_pdb_data, fasta_file, out_path, overwrite = overwrite, logger = logger)
+
+    # Get pairwise data for 2-mers and N-mers
+    pairwise_2mers_df = generate_pairwise_2mers_df(all_pdb_data, out_path = out_path, save_pairwise_data = save_pairwise_data, 
+                                                overwrite = overwrite, logger = logger)
+    pairwise_Nmers_df = generate_pairwise_Nmers_df(all_pdb_data, out_path = out_path, save_pairwise_data = save_pairwise_data, 
+                                                overwrite = overwrite, logger = logger)
+    
+    # Pack results
+    mm_output_preprocess = {
+        "prot_IDs": prot_IDs,
+        "prot_names": prot_names,
+        "prot_seqs": prot_seqs,
+        "prot_lens": prot_lens,
+        "prot_N": prot_N,
+        "out_path": out_path,
+        "log_level": log_level,
+        "all_pdb_data": all_pdb_data,
+        "sliced_PAE_and_pLDDTs": sliced_PAE_and_pLDDTs,
+        "pairwise_2mers_df": pairwise_2mers_df,
+        "pairwise_Nmers_df": pairwise_Nmers_df
+    }
+    
+    return mm_output_preprocess
+
+def run_stoich_expl_with_cutoff_list(mm_output_preprocess, cutoffs_list = cutoffs_list, 
+                                     use_dynamic_conv_soft_func = use_dynamic_conv_soft_func):
+
+    # Unpack data
+    log_level         = mm_output_preprocess['log_level']
+    base_out_path     = mm_output_preprocess['out_path']
+    pairwise_2mers_df = mm_output_preprocess['pairwise_2mers_df']
+    pairwise_Nmers_df = mm_output_preprocess['pairwise_Nmers_df']
+
+    logger = configure_logger(base_out_path, log_level = log_level)(__name__)
+
+    # To store results
+    conv_stoichs_dict = {}
+
+    for FPR, PAE_cutoff, N_models_cutoff in cutoffs_list:
+
+        logger.info( '   Running with cutoffs:')
+        logger.info(f'      - FPR: {FPR}')
+        logger.info(f'      - PAE cutoff: {PAE_cutoff}')
+        logger.info(f'      - Nº of models: {N_models_cutoff}')
+
+        # For 2-mers
+        pairwise_2mers_df_F3, unique_2mers_proteins = filter_non_int_2mers_df(
+            pairwise_2mers_df, 
+            min_PAE_cutoff = PAE_cutoff,
+            ipTM_cutoff = 0,
+            N_models_cutoff = N_models_cutoff)
+
+        # For N-mers
+        pairwise_Nmers_df_F3, unique_Nmers_proteins = filter_non_int_Nmers_df(
+            pairwise_Nmers_df,
+            min_PAE_cutoff_Nmers = PAE_cutoff,
+            pDockQ_cutoff_Nmers = 0,
+            N_models_cutoff = N_models_cutoff)
+
+        # Modify mm_output
+        mm_output_preprocess['out_path'] = base_out_path + f"/fpr{FPR}_pae{PAE_cutoff}_Nmodels{N_models_cutoff}"
+        mm_output_preprocess["pairwise_2mers_df_F3"] = pairwise_2mers_df_F3
+        mm_output_preprocess["pairwise_Nmers_df_F3"] = pairwise_Nmers_df_F3
+        mm_output_preprocess["unique_2mers_proteins"] = unique_2mers_proteins
+        mm_output_preprocess["unique_Nmers_proteins"] = unique_Nmers_proteins
+
+        # Make output dir
+        os.makedirs(mm_output_preprocess['out_path'], exist_ok = True)
+
+        # Run stoichiometric space exploration
+        stoich_dict, stoich_graph, removed_suggestions, added_suggestions, convergent_stoichiometries = generate_stoichiometric_space_graph(
+            mm_output_preprocess, suggested_combinations = [],
+            
+            # Variable cutoffs
+            use_dynamic_conv_soft_func = use_dynamic_conv_soft_func,
+            Nmers_contacts_cutoff_convergency = Nmers_contacts_cutoff_convergency,
+            N_models_cutoff = N_models_cutoff,
+            miPAE_cutoff_conv_soft = PAE_cutoff
+        )
+
+        # Save the results in a dict
+        dict_key_results = f'FPR={FPR}_Nmodels={N_models_cutoff}'
+        conv_stoichs_dict[dict_key_results] = {
+            "stoich_dict": stoich_dict,
+            "stoich_graph": stoich_graph,
+            "convergent_stoichiometries": convergent_stoichiometries
+        }
+
+        del dict_key_results, removed_suggestions, added_suggestions, pairwise_Nmers_df_F3, unique_Nmers_proteins, pairwise_2mers_df_F3, unique_2mers_proteins
+    
+    return conv_stoichs_dict
+
+def run_complex(fasta_file, AF2_2mers, AF2_Nmers, out_path, cutoff_list):
+
+    mm_output = preprocess_data(fasta_file, AF2_2mers, AF2_Nmers, out_path)
+
+    conv_stoichs_dict = run_stoich_expl_with_cutoff_list(mm_output, cutoff_list)
+
+    del mm_output
+
+    return conv_stoichs_dict
---
+++ tests/convergency/run_convergency_test.py
@@ -0,0 +1,104 @@
+
+import os
+
+from tests.convergency.convergency_test import run_complex
+
+######################################################################
+########################### Configurations ###########################
+######################################################################
+
+# (FPR, PAE_cutoff, N_models_cutoff)
+cutoffs_list = [
+
+    # ------------- Original cutoffs (DiscobaMultimer 20r) -------------
+
+    # # FPR = 0.01
+    # (0.01,  1.86, 1),
+    # (0.01,  1.89, 2),
+    # (0.01,  2.70, 3),
+    # (0.01,  4.21, 4),
+    # (0.01, 10.50, 5),
+    # # FPR = 0.05
+    # (0.05,  3.09, 1),
+    # (0.05,  4.42, 2),
+    # (0.05,  7.18, 3),
+    # (0.05, 10.44, 4),
+    # (0.05, 12.98, 5)
+
+    # ------------- New cutoff (AF2m + AF3 average) -------------
+
+    # FPR = 0.05
+    (0.05, 9.06, 4)
+]
+
+# Homooligomers files locations
+homooligomers_benchmark_dir = "/home/elvio/Desktop/homooligomeric_states_benchmark"
+
+homooligomers_ids = ['1A7K.4','1R26.1','2P7U.1','2RM5.1','3H4C.1','4BAS.1','5AB5.2',
+                     '5EUC.4','5F8V.2','6J9Q.2','7MK0.6','7S2H.2','7TUR.2','7V4J.5',
+                     '7WHR.6','7WP3.3','8AXJ.1','8I40.4','8J6E.2','8SF3.1']
+
+homo_complexes_dict = {}
+for id in homooligomers_ids:
+    homo_complexes_dict[id] = {
+        "fasta_file" : f'{homooligomers_benchmark_dir}/fastas/{id.split(".")[0]}.fasta',
+        "AF2_2mers"  : f'{homooligomers_benchmark_dir}/AF2_2mers/{id}',
+        "AF2_Nmers"  : f'{homooligomers_benchmark_dir}/AF2_Nmers/{id}'
+    }
+
+# Files location
+hetero_complexes_dict = {
+    "3MZL": {
+        "fasta_file" : '/home/elvio/Desktop/heteromultimeric_states_benchmark/converged_3MZL/proteins_mm.fasta',
+        "AF2_2mers"  : '/home/elvio/Desktop/heteromultimeric_states_benchmark/converged_3MZL/2-mers',
+        "AF2_Nmers"  : '/home/elvio/Desktop/heteromultimeric_states_benchmark/converged_3MZL/N-mers'
+    },
+    "Complex_2": {
+        "fasta_file" : None,
+        "AF2_2mers"  : None,
+        "AF2_Nmers"  : None
+    },
+}
+
+# Output directory for benchmark
+benchmark_dir = "/home/elvio/Desktop/stoichiometry_benchmark"
+os.makedirs(benchmark_dir, exist_ok=True)
+
+######################################################################
+############################# HELPER FX ##############################
+######################################################################
+
+def run_multiple_complexes(complexes_dict, cutoffs_list, benchmark_dir):
+
+    complexes_conv_stoichs_dict = {}
+
+    for comp in complexes_dict.keys():
+
+        conv_stoichs_dict = run_complex(
+            fasta_file  = complexes_dict[comp]['fasta_file'],
+            AF2_2mers   = complexes_dict[comp]['AF2_2mers'],
+            AF2_Nmers   = complexes_dict[comp]['AF2_Nmers'],
+            out_path    = f'{benchmark_dir}/{comp}',
+            cutoff_list = cutoffs_list
+        )
+
+        complexes_conv_stoichs_dict[comp] = conv_stoichs_dict
+
+    return complexes_conv_stoichs_dict
+
+######################################################################
+############################## Running ###############################
+######################################################################
+
+# TEST
+# comp = "3MZL"
+
+# conv_stoichs_dict = run_complex(
+#     fasta_file  = complexes_dict[comp]['fasta_file'],
+#     AF2_2mers   = complexes_dict[comp]['AF2_2mers'],
+#     AF2_Nmers   = complexes_dict[comp]['AF2_Nmers'],
+#     out_path    = f'{benchmark_dir}/{comp}',
+#     cutoff_list = cutoffs_list
+# )
+
+complexes_conv_stoichs_dict = run_multiple_complexes(homo_complexes_dict, cutoffs_list, benchmark_dir)
